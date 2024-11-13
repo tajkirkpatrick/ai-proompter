5665b00251e57f11b4b5d40e3d40ab4fb5cf5814
Taj K <taj.kirkpatrick@gmail.com>
2024-11-12 21:24:01 -0600

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "npm:astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false,
+  }), react()],
+});
