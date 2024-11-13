43a997f3b4646d640b1e679912e761de4c618cbd
Taj K <taj.kirkpatrick@gmail.com>
2024-11-12 22:16:32 -0600

    feat(index): accepts inputs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false,
-  }), react()],
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
